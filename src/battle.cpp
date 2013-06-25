162298e72e1ca8c196cb2354d34614eb98f1c7be
Michael Kristofik <kristo605@gmail.com>
2013-06-24 23:26:19 -0500

    Start parsing unit information from json file.

--- src/battle.cpp
+++ src/battle.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2012-2013 by Michael Kristofik <kristo605@gmail.com>
+    Copyright (C) 2013 by Michael Kristofik <kristo605@gmail.com>
     Part of the battle-sim project.
  
     This program is free software; you can redistribute it and/or modify
@@ -11,12 +11,40 @@
     See the COPYING.txt file for more details.
 */
 #include "sdl_helper.h"
+#include "team_color.h"
+#include <cassert>
 #include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <memory>
+#include "rapidjson/document.h"
+#include "rapidjson/filestream.h"
+
+#define BOOST_FILESYSTEM_NO_DEPRECATED
+#define BOOST_SYSTEM_NO_DEPRECATED
+#include "boost/filesystem.hpp"
+
+struct Unit
+{
+    std::string name;
+    std::string plural;
+    ImageSet img;
+    ImageSet reverseImg;
+    ImageSet animAttack;
+    ImageSet reverseAnimAttack;
+    int numAttackFrames;
+    ImageSet imgDefend;
+    ImageSet reverseImgDefend;
+};
 
 namespace
 {
     SdlSurface tile;
     SdlSurface grid;
+    std::unordered_map<std::string, Unit> allUnits;
 }
 
 void loadImages()
@@ -46,14 +74,97 @@ void drawMap()
     sdlBlit(grid, pixelFromHex(2, 4));
 }
 
+void loadUnitImage(const std::string &file,
+                   ImageSet &images, ImageSet &reversed)
+{
+    namespace bfs = boost::filesystem;
+    const bfs::path imagePath{"../img"};  // TODO: make this configurable
+
+    auto p = imagePath;
+    p /= file;
+    auto baseImg = sdlLoadImage(p.string());
+    if (baseImg) {
+        images = applyTeamColors(baseImg);
+        reversed = applyTeamColors(sdlFlipH(baseImg));
+    }
+    else {
+        std::cerr << "Error loading unit images: " << p.string() << '\n';
+    }
+}
+
+void loadUnit(const std::string &id, const rapidjson::Value &json)
+{
+    Unit u;
+    if (json.HasMember("name")) {
+        u.name = json["name"].GetString();
+    }
+    if (json.HasMember("plural")) {
+        u.plural = json["plural"].GetString();
+    }
+    if (json.HasMember("img")) {
+        loadUnitImage(json["img"].GetString(), u.img, u.reverseImg);
+    }
+    if (json.HasMember("img-defend")) {
+        loadUnitImage(json["img-defend"].GetString(), u.imgDefend,
+                      u.reverseImgDefend);
+    }
+    if (json.HasMember("anim-attack")) {
+    }
+    if (json.HasMember("anim-die")) {
+    }
+
+    allUnits.emplace(id, std::move(u));
+}
+
+bool parseJson()
+{
+    std::shared_ptr<FILE> fp{fopen("../data/units.json", "r"), fclose};
+    rapidjson::Document doc;
+    rapidjson::FileStream file(fp.get());
+    if (doc.ParseStream<0>(file).HasParseError()) {
+        std::cerr << "Error reading units: " << doc.GetParseError() << '\n';
+        return false;
+    }
+    if (!doc.IsObject()) {
+        std::cerr << "Expected top-level object in units file\n";
+        return false;
+    }
+
+    for (auto i = doc.MemberBegin(); i != doc.MemberEnd(); ++i) {
+        if (!i->value.IsObject()) {
+            std::cerr << "units: skipping tag '" << i->name.GetString() <<
+                "'\n";
+            continue;
+        }
+        std::cout << "\nUnit " << i->name.GetString() << '\n';
+        for (auto j = i->value.MemberBegin(); j != i->value.MemberEnd(); ++j) {
+            std::cout << j->name.GetString() << ' ' << j->value.GetString() << '\n';
+        }
+        loadUnit(i->name.GetString(), i->value);
+        const rapidjson::Value &v = i->value;
+        const rapidjson::Value &tag = v["img-defend"];
+        if (tag.IsNull()) {
+            std::cout << "Unit has no defend image\n";
+        }
+    }
+
+    return true;
+}
+
 extern "C" int SDL_main(int, char **)  // 2-arg form is required by SDL
 {
     if (!sdlInit(288, 360, "../img/icon.png", "Battle Sim")) {
         return EXIT_FAILURE;
     }
 
+    if (!parseJson()) {
+        return EXIT_FAILURE;
+    }
+
     loadImages();
     drawMap();
+
+    SDL_Surface *screen = SDL_GetVideoSurface();
     SDL_UpdateRect(screen, 0, 0, 0, 0);
 
     bool isDone = false;
