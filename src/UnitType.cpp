0c4fa208a7e7d6ffd06f9f6006012fde40ba75cf
Michael Kristofik <kristo605@gmail.com>
2014-01-30 16:53:28 -0600

    Added Sprite and Elf units.

--- src/UnitType.cpp
+++ src/UnitType.cpp
@@ -125,6 +125,7 @@ UnitType::UnitType(const rapidjson::Value &json)
     if (json.HasMember("growth")) {
         growth = json["growth"].GetInt();
     }
+    // TODO: warn if the unit has a ranged attack but no projectile.
     if (json.HasMember("projectile")) {
         projectile = sdlLoadImage(json["projectile"].GetString());
     }
